623b6db2ad516ed04fc937ab1a836feba86fe542
Sumit Rex <sumitrex666@gmail.com>
2026-08-23 16:44:54 +0530

    Build a counter using useState

--- react-practice-day-2/src/App.jsx
+++ react-practice-day-2/src/App.jsx
@@ -3,14 +3,25 @@ import {useState} from "react";
 const App = () => {
   const [count, setCount] = useState(0);
   const increaseCount = ()=>{
-      setCount(count+1);
-      console.log(count+1);
+      setCount((prev)=>prev+5);
+      console.log(count);
+  }
+  const decreaseCount = ()=>{
+      setCount((prev)=>prev-5);
+      console.log(count);
+  }
+  const resetCount = ()=>{
+      setCount(0);
+      console.log(count);
   }
 
   return (
     <div>
-      <h1>{count} </h1>
-      <button onClick={increaseCount}>Increment</button>
+      <h1 className="text-5xl font-bold">Counter</h1>
+      <h1 className="text-2xl font-bold">{count} </h1>
+      <button className="text-1xl font-bold border-2" onClick={increaseCount}>increase(+)</button>
+      <button className="text-1xl font-bold border-2" onClick={decreaseCount}>decrease(-)</button>
+      <button className="text-1xl font-bold border-2" onClick={resetCount}>reset</button>
     </div>
   )
 } 
