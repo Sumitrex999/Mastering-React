e201926ee4ad6627f7b518b5e9f02dc810ab9eee
Sumit Rex <sumitrex666@gmail.com>
2026-08-24 16:30:56 +0530

    Learn React and controlled inputs

--- react-practice-day-2/src/App.jsx
+++ react-practice-day-2/src/App.jsx
@@ -1,27 +1,45 @@
 import { useState } from "react";
 
 const App = () => {
+
+    const [todoText, setTodoText] = useState("");
+
     const [todos, setTodos] = useState([
         {id:1, title:"Learn React"},
         {id:2, title:"Learn JavaScript"},
     ]);
+
     const addItem = ()=>{
+        if(todoText.trim() === ""){
+          return;
+        }
         setTodos([
             ...todos,
-            {id:3, title:"Learn MERN"}
+            {id:3, title:todoText}
         ]);
+        setTodoText("");
     }
+
     const deleteTodo = (id)=>{
         setTodos(todos.filter((todo)=>(todo.id !== id)));
     }
+    
   return (
     <div>
       <h1>TODO LIST:</h1>
-      <input type="text" placeholder="Enter the Todo Item" />
+
+      <input 
+      type="text" 
+      placeholder="Enter the Todo Item" 
+      value={todoText}
+      onChange={(event)=> setTodoText(event.target.value)}
+      />
+
       <button onClick={addItem}>Add</button>
+      
       {todos.map((todo)=>(
         <div key={todo.id}>
-            <p>{todo.title} </p>
+            <p>{todo.title}</p>
             <button onClick={()=>{deleteTodo(todo.id)}}>Delete</button>
         </div>
       ))}
